63d77c9e8c29731b1da7899022a698a54773c439
Rochedo098 <pedroalabousas@gmail.com>
2021-11-26 21:35:41 -0300

    Optimize import, probably fix the build

--- src/main/kotlin/com/github/rochedo098/droids/screen/TheMachineUScreenHandler.kt
+++ src/main/kotlin/com/github/rochedo098/droids/screen/TheMachineUScreenHandler.kt
@@ -5,7 +5,6 @@ import com.github.rochedo098.droids.block.TheMachineBlockU
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.Inventory
-import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.slot.Slot
